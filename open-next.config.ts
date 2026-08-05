ef8a158000fe92756141e447827ec9d678ab5c61
maamspy <mdalaminmollah000@gmail.com>
2026-08-05 11:07:56 +0600

    chore: configure project for cloudflare opennext deployment

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+// For best results consider enabling R2 caching
+// See https://opennext.js.org/cloudflare/caching for more details
+// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+
+export default defineCloudflareConfig({});
